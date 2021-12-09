9bbfa5aac1cdf66dd48d320de033ce5d1c787596
YogiOnBioinformatics <yraghav97@gmail.com>
2021-12-09 12:49:58 -0500

    add script and output to get TMM normalized counts

---
+++ notebooks/diffbind/2_retrieve_count_matrix/scripts/get_matrix.R
@@ -0,0 +1,14 @@
+
+library(DiffBind)
+
+obj = dba(sampleSheet="/home/yraghav/AALS-Epigenomics-Signal-Reproducibility/notebooks/diffbind/pre-processing/output/sample_sheet.csv", minOverlap=.1)
+
+obj = dba.count(obj, minOverlap=.1, score=DBA_SCORE_TMM_READS_FULL, bParallel=TRUE)
+
+save(obj, file="/home/yraghav/Stas_Object.Rdata)
+
+write.table(dba.peakset(obj, DataType=DBA_DATA_FRAME, bRetrieve=TRUE), 
+           "/home/yraghav/AALS-Epigenomics-Signal-Reproducibility/notebooks/diffbind/2_retrieve_count_matrix/output/matrix.tsv", 
+           sep="\t", 
+           row.names=TRUE, 
+           col.names=TRUE)
